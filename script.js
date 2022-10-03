7e60438de3f19145256923f574c42c6d4de89056
Manamana <allentrobson@gmail.com>
2022-10-03 14:59:51 -0600

    Added comments to code

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
+// Element Variables 
 var rootEl = $('#root');
-
 var body = $('#middle');
-
 var textAreaEls = [];
 
 
 
-
+// Current Date and Time 
 var time = moment().format('LLLL');
 $("#currentDay").text(time);
 
+// Current Hour 
 var timeHour = moment().format('HH');
-// console.log(timeHour);
 
-// var testArray = ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7', 'Test 8', 'Test 9'];
-
-// localStorage.setItem('todo', JSON.stringify(testArray));
-
+// Array of text parsed from local storage
 var todoArray = JSON.parse(localStorage.getItem("todo"));
 
+// If local storage has never been used, sets array to non-null
 if (todoArray === null){
     todoArray = [""];
 
 }
 
+//Creates all of the body elements
 function createHour() {
 
     for (var i = 0; i < 9; i++) {
@@ -31,18 +29,15 @@ function createHour() {
 
         var div1 = $('<div>');
         var div2 = $('<div>');
-
         var span1 = $('<span>');
-
         var textarea = $('<textarea>');
-
         var div3 = $('<div>');
-
         var button1 = $('<button>');
-
+        
+        //Variable used to check if the current hour is the same as the selected hour
         var checkTime = i + 9;
 
-
+        //Body element styling attributes
         div1.attr('class', 'input-group');
         div2.attr('class', 'input-group-prepend');
         span1.attr('class', 'input-group-text');
@@ -58,14 +53,16 @@ function createHour() {
 
         textarea.attr('style','cursor:pointer');
 
-
+        // Sets each button to call the SaveText() function on-click. When created, its current index is
+        // saved as the parameter that is used when the function is called. This allows the saveText() function
+        // to know the index it should save to.
         button1.attr('onclick', 'saveText(' + i + ')');
 
+        //To-do text elements saved in array 
         textAreaEls.push(textarea);
 
-        // textarea.attr('style', 'color: rgb(0,0,0)');
-        // textarea.text("Test"+'\n'+'hello'+'\n'+'\n'+'\n'+'test'); 
-
+        
+        //Checks whether to say am or pm for current hour element
         if (i + 9 > 12) {
 
             span1.text((i + 9 - 12) + 'pm');
@@ -73,8 +70,8 @@ function createHour() {
 
             span1.text((i + 9) + 'am');
         }
-        console.log(checkTime + " " + timeHour);
-
+        
+        //Sets the color of the current text element based on whether it is before current time, equal to, or after.
         if (checkTime < timeHour) {
             textarea.addClass('bg-secondary');
         } else if (checkTime == timeHour) {
@@ -84,11 +81,13 @@ function createHour() {
 
         }
 
+        //Sets text to stored text
         textarea.text(todoArray[i]);
 
-
+        //Save button text
         button1.text('Save');
 
+        //Appends elements
         div3.append(button1);
         div2.append(span1);
 
@@ -104,23 +103,17 @@ function createHour() {
 }
 
 
-
+//Calls function to create body.
 createHour();
 
 
-
+//Saves text entered
 function saveText(index) {
 
 
     todoArray[index] = textAreaEls[index].val();
     
-
-    // console.log(todoArray[index]);
-    // console.log(textAreaEls[index].val());
-    
-    // todoArray[index] = textAreaEls[index].text;
-    // console.log();
-   
+ 
 
 
 
